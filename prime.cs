66902b5aa2cc4cd3991021928ce25c33ed45b1cb
Kamran-Sohail <106171774+Kamran-Sohail@users.noreply.github.com>
2024-02-16 01:47:57 +0500

    Add files via upload

---
+++ prime.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Prime_Number
+{
+    class Prime
+    {
+        static void Main(string[] args)
+        {
+            bool Is_Prime = true;
+            Console.WriteLine("Prime Numbers : ");
+            for (int i = 2; i <= 100; i++)
+            {
+                for (int j = 2; j <= 100; j++)
+                {
+
+                    if (i != j && i % j == 0)
+                    {
+                        Is_Prime = false;
+                        break;
+                    }
+
+                }
+                if (Is_Prime)
+                {
+                    Console.Write("\t" + i);
+                }
+                Is_Prime = true;
+            }
+            Console.ReadKey();
+        }
+    }
+}
